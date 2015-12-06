cac4171d948941a37e10be0171afe505f1684b14
Artur Sapek <me@artur.co>
2015-12-06 14:13:10 -0500

    Handle presetColorScheme opt

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import URI from 'urijs';
+
 // TODO generate this and corresponding validCurrencyPairs
 // using config from go codebase
 const validExchanges = [
@@ -27,6 +29,10 @@ const validTimePeriods = [
   '1', '3', '5', '15', '30', '1H', '2H', '4H', '6H', '12H', '1D', '3D', '1W'
 ]
 
+const requiredColorSchemeKeys = [
+    'bg', 'text', 'textStrong', 'textWeak', 'short', 'shortFill', 'long', 'longFill', 'cta', 'ctaHighlight', 'alert'
+]
+
 class Embed {
   constructor(exchange, currencyPair, opts={}) {
     this.exchange = exchange;
@@ -45,6 +51,8 @@ class Embed {
     if (currencyPair === undefined) {
       throw new Error('currencyPair required');
     }
+    // TODO validate that exchange supports this currencyPair (generated config)
+    // As this is now, it will just render an iframe containing the 404 page
 
     if (opts.timePeriod !== undefined) {
       if (validTimePeriods.indexOf(opts.timePeriod) === -1) {
@@ -53,10 +61,16 @@ class Embed {
     } else {
       opts.timePeriod = '1H';
     }
+
+    opts.timePeriod = opts.timePeriod.toLowerCase();
   }
 
   get src() {
-    return `https://embed.cryptowat.ch/${this.exchange}/${this.currencyPair}/${this.opts.timePeriod}`
+    let uri = URI(`https://embed.cryptowat.ch/${this.exchange}/${this.currencyPair}/${this.opts.timePeriod}`);
+    if (this.opts.presetColorScheme !== undefined) {
+      uri.query({ presetColorScheme: this.opts.presetColorScheme });
+    }
+    return uri.toString()
   }
 
   mount(elem) {
--- tests/index.js
+++ tests/index.js
@@ -2,11 +2,25 @@ var assert = require('assert');
 var Embed = require('../dist/main.js');
 
 describe('Embed', function () {
-  it('Generates the correct iframe src', function() {
+  it('generates the default URL when given no opts', function() {
     assert.equal(
       (new Embed('bitfinex', 'btcusd').src),
-      'https://embed.cryptowat.ch/bitfinex/btcusd'
+      'https://embed.cryptowat.ch/bitfinex/btcusd/1h'
     );
-    
   });
+
+  it('handles the timePeriod opt', function() {
+    assert.equal(
+      (new Embed('bitfinex', 'btcusd', { timePeriod: '6H' }).src),
+      'https://embed.cryptowat.ch/bitfinex/btcusd/6h'
+    );
+  });
+
+  it('handles the presetColorScheme opt', function() {
+    assert.equal(
+      (new Embed('bitfinex', 'btcusd', { presetColorScheme: 'albuquerque' }).src),
+      'https://embed.cryptowat.ch/bitfinex/btcusd/1h?presetColorScheme=albuquerque'
+    );
+  });
+
 });
